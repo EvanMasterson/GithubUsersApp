c39fdccd0cf6f68d570793c3a8fcacd422e36d34
EvanMasterson <evanm95@hotmail.com>
2020-09-21 16:30:23 +0100

    create blank project

---
+++ GithubUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubUsers
+//
+//  Created by Evan Masterson on 21/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
